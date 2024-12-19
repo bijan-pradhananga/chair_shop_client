6701c580201aa3d029389ed9e38438da0d80dea9
Bijan <bijan2059@gmail.com>
2024-12-19 19:04:03 +0545

    add navbar

--- app/(client)/layout.js
+++ app/(client)/layout.js
@@ -1,9 +1,11 @@
+import Navbar from "@/components/nav-bar";
+
 export default function Layout({ children }) {
     return (
-        <div className="min-h-screen flex flex-col">
-                <nav>navbar</nav>
+        <div >
+                <Navbar/>
                 {/* Main Content */}
-                <main className="flex-grow container mx-auto py-8 px-4">
+                <main className="mx-auto max-w-2xl px-4 py-5 sm:px-6 lg:max-w-7xl lg:px-8">
                 
                     {children}
 
