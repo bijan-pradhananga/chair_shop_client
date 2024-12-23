baf17b757c83d981ea1b052ab74f13ab1c20988e
Bijan <bijan2059@gmail.com>
2024-12-23 19:14:39 +0545

    update contact info

--- app/(client)/contact/contactInfo.js
+++ app/(client)/contact/contactInfo.js
@@ -1,5 +1,6 @@
 export const contactInfo = {
     details: `Got a technical issue? Want to send feedback about our site? Need
             details about our products? Let us know.`,
-
+    phone: "9843149918",
+    email: "thechairshop304@gmail.com"
 }
