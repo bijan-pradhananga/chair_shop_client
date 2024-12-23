ac9c618c31611eca4aca3a2728b27406729ec086
Bijan <bijan2059@gmail.com>
2024-12-23 19:14:52 +0545

    update contact page

--- app/(client)/contact/page.jsx
+++ app/(client)/contact/page.jsx
@@ -68,8 +68,12 @@ const Header = () => {
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
                 Contact Us
             </h2>
-            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            <p className="mb-2 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
                 {contactInfo.details}
+            </p>
+            <p className="mb-2 font-semibold text-gray-500 dark:text-gray-400 sm:text-lg  text-center">
+                Contact Number: {contactInfo.phone} <br />
+                Email: {contactInfo.email}
             </p>
         </>
     )
