b9bb89678c2591c5210b142b6b08dedf203c3e6a
Bijan <bijan2059@gmail.com>
2024-12-25 17:32:24 +0545

    update order page

--- app/(admin)/admin/orders/page.jsx
+++ app/(admin)/admin/orders/page.jsx
@@ -17,6 +17,8 @@ import AlertSuccess from "@/components/alert-success";
 import AlertFailure from "@/components/alert-failure";
 import OrderPaginationComponent from "@/components/order-pages";
 import { useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const OrderPage = () => {
 
@@ -78,8 +80,10 @@ const OrderPage = () => {
                 <TableRow>
                   <TableHead className="w-[100px]">ID</TableHead>
                   <TableHead>Date</TableHead>
-                  <TableHead>Total Amount</TableHead>
+                  <TableHead>Payment</TableHead>
+                  <TableHead>Total</TableHead>
                   <TableHead>Status</TableHead>
+                  <TableHead>Payment <br /> Status</TableHead>
                   <TableHead >Action</TableHead>
                 </TableRow>
               </TableHeader>
@@ -88,16 +92,29 @@ const OrderPage = () => {
                   <TableRow key={index}>
                     {/* Serial Number */}
                     <TableCell className="font-medium">{item._id}</TableCell>
-                    {/* Name */}
-                    <TableCell>{new Date(item.createdAt).toLocaleDateString("en-CA").replace(/-/g, ".")}</TableCell>
-                    {/* Description */}
-                    <TableCell className="max-w-xs">{item.totalAmount}</TableCell>
                     {/* Date */}
+                    <TableCell>{new Date(item.createdAt).toLocaleDateString("en-CA").replace(/-/g, ".")}</TableCell>
+                    {/* Payment Method  */}
+                    <TableCell className="max-w-xs">{item.billingInfo.paymentMethod}</TableCell>
+                    {/* Amount */}
+                    <TableCell className="max-w-xs">Rs.{item.totalAmount}</TableCell>
+                    {/* Status */}
                     <TableCell>{item.status}</TableCell>
+                    {/* Payment Status  */}
+                    <TableCell className="max-w-xs">{item.paymentStatus}</TableCell>
                     {/* Actions */}
                     <TableCell>
                       <div className="flex items-center gap-2">
-                        {item.status === 'Pending' ? (
+                
+                          <Link
+                            href={`/admin/orders/${item._id}`}
+                            className="w-full inline-flex justify-center rounded-lg  border border-gray-200 bg-white px-3 py-1 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:w-auto"
+                          >
+                            View
+                          </Link>
+
+                    
+                        {item.status === 'Pending' && (
                           <>
                             <button
                               onClick={() => handleConfirmOrder(item._id)}
@@ -112,8 +129,6 @@ const OrderPage = () => {
                               Cancel
                             </button>
                           </>
-                        ) : (
-                          <p className="text-gray-500">No further actions available</p>
                         )}
 
                       </div>
@@ -166,10 +181,10 @@ const Header = ({ fetchItems, searchItems }) => {
 }
 
 const Page = () => {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <OrderPage />
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderPage />
+    </Suspense>
+  );
 };
 export default Page
